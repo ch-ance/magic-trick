7b721473df39f0e057dedf6d76128da563a11969
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2022-01-27 17:38:49 -0600

    updates

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function main() {
   console.log(
     "Hey Chance!! Hope your day is going well. Sincerely, your friend, Chance."
   );
+  console.log(
+    `I updated the package! It's currently ${Date.now().toLocaleString()}`
+  );
 }
 
 main();
